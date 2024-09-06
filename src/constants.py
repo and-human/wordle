b59a1f82b0789d9a642356b8f285e4d30a20efc8
Anshuman Sharma <69304992+and-human@users.noreply.github.com>
2024-09-07 04:19:50 +0530

    updating minor changes

---
+++ src/constants.py
@@ -0,0 +1,43 @@
+#Colours
+BG_COLOR                = 34, 36, 34        #Black
+TEXT_COLOR              = 255, 255, 255           #White
+EMPTY_BOX_COLOR         = 128, 128, 128     #Grey
+SUCCESS_BOX_COLOR       = 0, 128, 0         #Green
+INCORRECT_BOX_COLOR     = 128, 128, 128     #Grey
+PARTIAL_BOX_COLOR       = 255, 255, 0       #Yellow
+EMPTY_BOX_FONT_COLOR    = 255, 255, 255           #Black
+SUCCESS_BG_COLOR        = 0, 128, 0
+INCORRECT_BG_COLOR      = 128, 0, 0
+
+
+
+#Dimensions
+WIDTH = 1470
+HEIGHT = 956
+BOX_WIDTH = 50
+BOX_SIZE = 50
+ROW_GUTTER = 7
+WINDOW_WIDTH = 200
+WINDOW_HEIGHT = 50
+
+
+#Texts
+TITLE = "Wordle"
+FONT = "Arial"
+BOX_FONT_SIZE = 35
+WINDOW_FONT_SIZE = 24
+
+
+#MISC
+ATTEMPTS = 6
+WORD_LENGTH = 5
+WORDS_LIST = 'words/test_bank.txt'
+
+
+#Box States
+EMPTY = 0
+INCORRECT = 1
+PARTIAL = 2
+SUCCESS = 3
+NOT_FOUND = 4
+
