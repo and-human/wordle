3cf1079132dc7335124f925a2f03f03809bf73a7
Anshuman Sharma <69304992+and-human@users.noreply.github.com>
2024-09-07 02:56:35 +0530

    main game class controlling UI and logic

---
+++ src/game.py
@@ -0,0 +1,106 @@
+import random
+import pygame as pg
+import os
+from .constants import *
+from .gridLine import GridLine
+from .box import Box
+from .game_states import *
+
+
+class Game:
+    def __init__(self, screen, word_length: int, word_list: list):
+        self.screen = screen
+        self.word_length = word_length
+        self.word_list = word_list
+        self.game_state = PlayState()
+
+        self.game_matrix = []
+        self.target_word = word_list[random.randint(0, len(word_list) - 1)].upper()
+
+        # Counter variables for keeping track of letters entered
+        self.current_line   = 0
+        self.current_letter = 0
+
+        # Creating the text boxes for the game
+        self._create_text_boxes()
+
+    def write_letter(self, letter: str):
+        """
+        Write the letter in the box
+        """
+        if self.current_letter < self.word_length:
+            letter = letter.upper()
+            self.game_matrix[self.current_line][self.current_letter].set_text(letter, self.screen)
+            self.current_letter += 1
+
+    def delete_letter(self):
+        """
+        Delete the letter in the box
+        """
+        if self.current_letter > 0:
+            self.current_letter -= 1
+            self.game_matrix[self.current_line][self.current_letter].delete_text(self.screen)
+
+    def enter_word(self):
+        """
+        Enter the word in the box
+        """
+        if self.current_letter == self.word_length:
+            word_result = self._validate_word()
+
+            # Checking the result of the word
+            if word_result == SUCCESS:
+                self.game_state = WinState()
+
+            elif word_result == INCORRECT:
+                self.game_state = LoseState()
+
+            elif word_result == NOT_FOUND:
+                self.game_state = WordNotFound()
+
+            elif self.current_line == ATTEMPTS - 1:
+                self.game_state = LoseState()
+
+            else:
+                self.game_state = PlayState()
+                self.current_line += 1
+                self.current_letter = 0
+
+    def _create_text_boxes(self):
+        """
+        Create the text boxes for the game
+        """
+        for i in range(int(ATTEMPTS)):
+            row = []
+
+            # Creating boxes for each letter in a row
+            for j in range(self.word_length):
+                pos = (WIDTH * 0.34) + ROW_GUTTER + (j * ROW_GUTTER + j * BOX_SIZE), (HEIGHT * 0.1) + (i * ROW_GUTTER + i * BOX_SIZE)
+
+                # Creating an empty box for the letter
+                unit_box = Box(pos, BOX_SIZE, 0)
+                unit_box.draw(self.screen)
+                row.append(unit_box)
+
+            self.game_matrix.append(row)
+    
+    def _validate_word(self):
+        """
+        Validate the word entered by the user
+        """
+        pass
+            
+
+if __name__ == "__main__":
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    game = Game(screen, WORD_LENGTH, ['hello', 'world', 'python'])
+    pg.display.update()
+    running = True
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        pg.display.update()
